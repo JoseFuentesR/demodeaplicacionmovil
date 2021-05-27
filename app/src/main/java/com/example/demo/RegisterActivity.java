c1a06211b3cf429b9047cfd849769c78a2fa4ae0
Panda121 <caca.candente@gmail.com>
2021-05-27 04:54:17 -0400

    hay que arreglar los botones y todo

---
+++ app/src/main/java/com/example/demo/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+
+    public void createAccount(View view) {
+    }
+
+    public void launchLogin(View view) {
+    }
+}
